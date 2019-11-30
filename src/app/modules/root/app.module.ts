fd35f5cc427bd10bb85fb26489efb68604ef84eb
Dimitar Dimitrov <d.zhivkov1990@gmail.com>
2019-11-30 09:30:49 +0200

    Minor changes in app.module (reorder imports)

--- src/app/modules/root/app.module.ts
+++ src/app/modules/root/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './/modules/app-routing.module';
+import { MaterialModule } from './modules/material/material.module';
+
 import { AppComponent } from './components/app-component/app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { OffersComponent } from './components/offers/offers.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { OffersService } from './services/http-services/offers-service/offers.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './modules/material/material.module';
 import { OfferDetailsComponent } from './components/offer-details/offer-details.component';
+
+import { OffersService } from './services/http-services/offers-service/offers.service';
 
 @NgModule({
   declarations: [
